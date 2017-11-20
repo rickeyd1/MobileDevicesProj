bb85f9d91e27fcd642a3794d2febc8c627f2f775
Property404 <daganboy@gmail.com>
2017-11-20 09:10:29 -0500

    Abstract away SQL tables

--- app/src/main/java/com/mba/drc/medicalapp/DBHelper.java
+++ app/src/main/java/com/mba/drc/medicalapp/DBHelper.java
@@ -4,11 +4,51 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.Cursor;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.ArrayList;
 
 
 public class DBHelper extends SQLiteOpenHelper {
+
+    // Abstract away SQL tables so we have to do less
+    // string concatenation bullshit
+    private class Table{
+        private Map<String, String> _map;
+        private ArrayList<String> _key_mirror;
+        private String _name;
+
+        Table(String name){
+            _map = new LinkedHashMap<String, String>();
+            _name = name;
+        }
+
+        // New table column (don't call this after you create the table)
+        void addColumn(String columnName, String type){
+            _map.put(columnName, type);
+            _key_mirror.add(columnName);
+        }
+
+        // Return where the column is (in a list of columns)
+        int columnIndex(String columnName){
+            return _key_mirror.indexOf(columnName);
+        }
+
+        // export as CREATE TABLE SQL query
+        String asQuery(){
+            String output = "CREATE TABLE IF NOT EXISTS "+_name+" (";
+            Boolean passedFirst = false;
+            for(String key: _map.values()){
+                if(passedFirst){
+                    output += ",";
+                }
+                output += key + " " + _map.get(key);
+                passedFirst = true;
+            }
+            output += ")";
+            return output;
+        }
+    }
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "MedicalAppMBA";
     private static final String DRUG_TABLE_NAME = "DrugAlarm";
@@ -23,53 +63,34 @@ public class DBHelper extends SQLiteOpenHelper {
     private static final String KEY_TIME_RESPONDED = "time_responded";
     private static final String KEY_DRUG_ID = "drug_alarm_id";
     private static final String KEY_RESPONSE = "response";
+    private Table drug_table;
+    private Table event_table;
 
-    public DBHelper(Context context){
+    public DBHelper(Context context)
+    {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
-    }
 
-    class TableCreator{
-        private Map<String, String> _map;
-        private String _name;
-        TableCreator(String name){
-            _map = new HashMap<String, String>();
-            _name = name;
-        }
-        public void setName(String name){_name = name;}
-        public void addColumn(String columnName, String type){
-            _map.put(columnName, type);
-        }
-        public String asQuery(){
-            String output = "CREATE TABLE IF NOT EXISTS "+_name+" (";
-            Boolean passedFirst = false;
-            for(String key: _map.values()){
-                if(passedFirst){
-                    output += ",";
-                }
-                output += key + " " + _map.get(key);
-                passedFirst = true;
-            }
-            output += ")";
-            return output;
-        }
-    }
-    @Override
-    public void onCreate(SQLiteDatabase db){
-        TableCreator drug_table = new TableCreator(DRUG_TABLE_NAME);
+        drug_table = new Table(DRUG_TABLE_NAME);
         drug_table.addColumn(KEY_ID, "INTEGER PRIMARY KEY");
         drug_table.addColumn(KEY_NAME, "TEXT");
         drug_table.addColumn(KEY_TIME, "INTEGER");
         drug_table.addColumn(KEY_DAYS, "INTEGER");
         drug_table.addColumn(KEY_DOSAGE, "TEXT");
         drug_table.addColumn(KEY_URGENCY, "INTEGER");
-        db.execSQL(drug_table.asQuery());
 
-        TableCreator event_table = new TableCreator(EVENT_TABLE_NAME);
+        event_table = new Table(EVENT_TABLE_NAME);
         event_table.addColumn(KEY_ID, "INTEGER PRIMARY KEY");
         event_table.addColumn(KEY_TIME_OCCURRED, "INTEGER");
         event_table.addColumn(KEY_TIME_RESPONDED, "INTEGER");
         event_table.addColumn(KEY_DRUG_ID, "INTEGER");
         event_table.addColumn(KEY_RESPONSE, "INTEGER");
+    }
+
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        db.execSQL(drug_table.asQuery());
         db.execSQL(event_table.asQuery());
     }
 
