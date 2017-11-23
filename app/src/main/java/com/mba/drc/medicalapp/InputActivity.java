85749d0d54ac60c52b9bcb8f9195674abd0fa819
Property404 <daganboy@gmail.com>
2017-11-23 12:35:30 -0500

    Use TimePicker instead of EditText for time input

--- app/src/main/java/com/mba/drc/medicalapp/InputActivity.java
+++ app/src/main/java/com/mba/drc/medicalapp/InputActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.CheckBox;
+import android.widget.TimePicker;
 
 /**
  * Created by dagan on 11/23/17.
@@ -14,7 +15,7 @@ import android.widget.CheckBox;
 public class InputActivity extends AppCompatActivity {
     DBHelper dbHelper;
     EditText nameET;
-    EditText timeET;
+    TimePicker timeTP;
     CheckBox sunCB;
     CheckBox monCB;
     CheckBox tuesCB;
@@ -31,10 +32,9 @@ public class InputActivity extends AppCompatActivity {
         setContentView(R.layout.input);
 
         nameET = findViewById(R.id.drug_et);
-        timeET = findViewById(R.id.time_et);
+        timeTP = findViewById(R.id.time_tp);
         dosageET = findViewById(R.id.dosage_et);
         urgencyRG = findViewById(R.id.urgency_rg);
-
 
         sunCB = findViewById(R.id.sunCheck);
         monCB = findViewById(R.id.monCheck);
@@ -44,12 +44,11 @@ public class InputActivity extends AppCompatActivity {
         friCB = findViewById(R.id.friCheck);
         satCB = findViewById(R.id.satCheck);
 
-
-
         dbHelper = new DBHelper(this);
     }
 
     public void add(View view){
+
 
     }
 }
