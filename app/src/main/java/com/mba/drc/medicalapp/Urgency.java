cd6af8add0bfd681787b71eea65f32a9dce96169
Property404 <daganboy@gmail.com>
2017-11-23 23:12:35 -0500

    Fix urgency bug

--- app/src/main/java/com/mba/drc/medicalapp/Urgency.java
+++ app/src/main/java/com/mba/drc/medicalapp/Urgency.java
@@ -5,7 +5,7 @@ package com.mba.drc.medicalapp;
  */
 
 public enum Urgency {
-    LOW_URGENCY(1), MID_URGENCY(2), HIGH_URGENCY(4);
+    LOW_URGENCY(0), MID_URGENCY(1), HIGH_URGENCY(2);
 
     private final int mValue;
     Urgency(int value){
@@ -13,6 +13,19 @@ public enum Urgency {
     }
     int value(){
         return mValue;
+    }
+
+    public String toString(){
+        switch(mValue){
+            case 0:
+                return "LOW_URGENCY";
+            case 1:
+                return "MID_URGENCY";
+            case 2:
+                return "HIGH_URGENCY";
+            default:
+                return "NULL_URGENCY";
+        }
     }
 }
 
