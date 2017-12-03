0574b0cc90df9c43e367a714d06e2e173bb92e6e
Property404 <daganboy@gmail.com>
2017-12-03 11:01:12 -0500

    Inflate menu

--- app/src/main/java/com/mba/drc/medicalapp/MainActivity.java
+++ app/src/main/java/com/mba/drc/medicalapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.mba.drc.medicalapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import java.util.List;
+
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -57,5 +60,24 @@ public class MainActivity extends AppCompatActivity {
         inputActivity.putExtra(INTENT_EXTRA_UPDATE_DRUG_ALARM, true);
         inputActivity.putExtra(INTENT_EXTRA_DRUG_ALARM_ID, drugId);
         startActivity(inputActivity);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if(id == R.id.menuitem_Settings){
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
