6ae94500123fd3d9fd9e634d25fce88a3de66fa7
rickeyd1 <rickjay0@gmail.com>
2017-12-02 22:42:48 -0500

    Added the imports hope I didnt forget anythin

--- app/src/main/java/com/mba/drc/medicalapp/MainActivity.java
+++ app/src/main/java/com/mba/drc/medicalapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.mba.drc.medicalapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.app.ActionBar;
+import android.view.Menu;
+import android.view.MenuItem;
 import java.util.List;
 import android.view.View;
 import android.widget.AdapterView;
